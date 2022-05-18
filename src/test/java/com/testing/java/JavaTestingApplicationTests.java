94bed194471f1f0bb0b39a8c76b7de9edf55c234
Patrick <nyakoria@gmail.com>
2022-05-18 20:24:35 +0300

    First commit

---
+++ src/test/java/com/testing/java/JavaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
